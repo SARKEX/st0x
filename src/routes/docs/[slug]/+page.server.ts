fd783c66a5d18d9b11ac6ace2f18d36d3061811e
Siddharth2207 <siddharthbhoite22@gmail.com>
2025-06-24 12:26:25 +0530

    fix test, build

--- src/routes/docs/[slug]/+page.server.ts
+++ src/routes/docs/[slug]/+page.server.ts
@@ -2,17 +2,17 @@ import type { EntryGenerator } from './$types';
 import { base } from '$app/paths';
 import slugFromPath from '$lib/docs/slugFromPath';
 
-export const entries: EntryGenerator = () => {
-	const modules = import.meta.glob(`/src/docs/**/*.{md,svx,svelte.md}`);
-
-	const paths: { slug: string }[] = [];
+export const entries: EntryGenerator = async () => {
+	const modules = import.meta.glob('/src/docs/**/*.{md,svx,svelte.md}');
+	const slugs: { slug: string }[] = [];
 
 	for (const path of Object.keys(modules)) {
 		const adjustedPath = `${base}${path.replace('/src/docs', '')}`;
-		paths.push({ slug: slugFromPath(adjustedPath) || '' });
+		const slug = slugFromPath(adjustedPath);
+		if (slug) slugs.push({ slug });
 	}
 
-	return paths;
+	return slugs;
 };
 
 export const prerender = true;
