7c6b84bdbd5be55a31d0b81c382fe92c4e29ad66
Siddharth2207 <siddharthbhoite22@gmail.com>
2025-07-07 12:21:15 +0530

    test

--- src/lib/addSchemaToReceipts.ts
+++ src/lib/addSchemaToReceipts.ts
@@ -1,8 +1,7 @@
 import { MAGIC_NUMBERS } from './consts';
 import { cborDecode, bytesToMeta } from './helpers';
 import type {
-	OffchainAssetReceiptVault,
-	ReceiptVaultInformation
+	OffchainAssetReceiptVault
 } from './types/OffchainAssetReceiptVault';
 
 export const addSchemaToReceipts = (vault: OffchainAssetReceiptVault) => {
@@ -38,109 +37,3 @@ export const addSchemaToReceipts = (vault: OffchainAssetReceiptVault) => {
 	}
 	return tempSchema;
 };
-
-if (import.meta.vitest) {
-	const { it, expect, vi } = import.meta.vitest;
-
-	vi.mock('$lib/scripts/helpers', async (importOriginal) => {
-		return {
-			...((await importOriginal()) as object),
-			cborDecode: vi.fn(),
-			bytesToMeta: vi.fn()
-		};
-	});
-
-	const mockVault: OffchainAssetReceiptVault = {
-		address: '0x123',
-		symbol: 'TST',
-		totalShares: '',
-		withdraws: [],
-		deposits: [],
-		tokenHolders: [],
-		certifications: [],
-		rolesGranted: [],
-		roleRevokes: [],
-		receiptVaultInformations: [
-			{
-				information:
-					'0xff0a89c674ee7874a4005841789cab56ca482cce50b2520acc4d290df0f035f34e4df4c90f288c348c2ac90d8c74b1f04d35cf4f2b4a7573f7720e0c74f10d4c8bcac80e4d54aa0500244c1305011bff8cd2927c8c86cb02706170706c69636174696f6e2f6a736f6e03676465666c617465a200782e516d647550484d364b65614c6f507159315a746d515944384d65376f66726546474a435151444d51665a686b5561011bff9fae3cc645f463',
-				timestamp: '1620000000',
-				id: '1'
-			}
-		] as unknown as ReceiptVaultInformation
-	} as unknown as OffchainAssetReceiptVault;
-
-	it('should add schema to receipts', () => {
-		vi.resetAllMocks();
-		const expectedSchema = [
-			{
-				displayName: 'Test Schema',
-				timestamp: '1620000000',
-				id: '1',
-				hash: 'abcdef'
-			}
-		];
-
-		vi.mocked(cborDecode).mockReturnValue([
-			// @ts-expect-error - Map type is not defined
-			new Map([
-				[1, MAGIC_NUMBERS.OA_SCHEMA],
-				[0, 'Test Schema']
-			]),
-			new Map([[0, 'abcdef']])
-		]);
-
-		vi.mocked(bytesToMeta).mockReturnValue({
-			displayName: 'Test Schema'
-		});
-
-		const result = addSchemaToReceipts(mockVault);
-		expect(result).toEqual(expectedSchema);
-	});
-
-	it('should return empty array if no receiptVaultInformations', () => {
-		vi.resetAllMocks();
-
-		const emptyVault: OffchainAssetReceiptVault = {
-			address: '0x123',
-			symbol: 'TST',
-			totalShares: '',
-			withdraws: [],
-			deposits: [],
-			tokenHolders: [],
-			certifications: [],
-			rolesGranted: [],
-			roleRevokes: [],
-			receiptVaultInformations: []
-		} as unknown as OffchainAssetReceiptVault;
-
-		const result = addSchemaToReceipts(emptyVault);
-		expect(result).toEqual([]);
-	});
-
-	it('should return empty array if no valid schema is found', () => {
-		vi.resetAllMocks();
-
-		const mockVaultWithInvalidSchema: OffchainAssetReceiptVault = {
-			address: '0x123',
-			symbol: 'TST',
-			totalShares: '',
-			withdraws: [],
-			deposits: [],
-			tokenHolders: [],
-			certifications: [],
-			rolesGranted: [],
-			roleRevokes: [],
-			receiptVaultInformations: [
-				{
-					information: '0x',
-					timestamp: '1630000000',
-					id: '1'
-				}
-			] as unknown as ReceiptVaultInformation
-		} as unknown as OffchainAssetReceiptVault;
-
-		const result = addSchemaToReceipts(mockVaultWithInvalidSchema);
-		expect(result).toEqual([]);
-	});
-}
